8c647d4e24175fd53ec80ed624a0c537fc0a04af
Leonardo <leonardoparisi7@gmail.com>
2022-01-20 19:45:44 +0100

    implented zoom function

--- app/src/main/java/com/example/gallery/MainActivity.kt
+++ app/src/main/java/com/example/gallery/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.gallery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import com.aghajari.zoomhelper.ZoomHelper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +11,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        return ZoomHelper.getInstance().dispatchTouchEvent(ev!!,this) || super.dispatchTouchEvent(ev)
     }
 }
